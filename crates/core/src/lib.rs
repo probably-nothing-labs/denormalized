c5840822acaffeca6546f19e891a37c88ded3008
Matt Green <emgeee@users.noreply.github.com>
2024-08-23 10:57:56 -0700

    add prelude to core

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -10,3 +10,21 @@ pub mod planner;
 pub mod query_planner;
 pub mod state_backend;
 pub mod utils;
+
+// re-export datafusion
+pub use datafusion;
+
+// re-export common module
+pub mod common {
+    pub use denormalized_common::*;
+}
+
+pub mod prelude {
+    pub use crate::context::Context;
+    pub use crate::datastream::DataStream;
+    pub use crate::physical_plan::utils::time::TimestampUnit;
+
+    pub use denormalized_common::{DenormalizedError, Result};
+
+    pub use datafusion::dataframe::DataFrame;
+}
